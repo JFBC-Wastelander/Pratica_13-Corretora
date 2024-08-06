867e9cc626e04dfb0e52034afb9d1b649b9fac1b
Jean <JFBimCopinski@gmail.com>
2024-08-06 11:34:36 -0300

    feat: lógica

--- index.js
+++ index.js
@@ -1,31 +1,140 @@
-const prompt = require("prompt-sync")()
-const acoes = require("./revendedora.js")
+const prompt = require("prompt-sync")();
+const corretora = require("./modulos/corretora.js");
+const cliente = require("./modulos/cliente.js");
+const corretor = require("./modulos/corretor.js");
 
-while (true) {
-console.log(`Tecle 1 para criar
-Tecle 2 para listar
-Tecle 3 para atualizar
-Tecle 4 para deletar
-Tecle 5 para sair`)
+const menuPrincipal = () => {
+  console.log(`GERENCIAMENTO DE CORRETORA DE IMÓVEIS.\n`);
+  while (true) {
+    console.log(`ESCOLHA O MÓDULO QUE QUER GERENCIAR:
+  
+1- CORRETORA
+2- CLIENTE
+3- CORRETOR
+0- SAIR\n`);
 
-let entrada = +prompt("O que deseja fazer? ")
+    let entrada = parseInt(prompt(": "));
 
-switch (entrada) {
-    case 1:
-        acoes.criarImovel()
-        break
-    case 2:
-        acoes.listarImovel()
-        break
-    case 3:
-        acoes.atualizarImovel()
-        break
-    case 4:
-        acoes.deletarImovel()
-        break
-    case 5:
-        console.log("Saindo . . .")
-        process.exit()
-        break
-}
-}
+    switch (entrada) {
+      case 1:
+        MenuCorretora();
+        break;
+      case 2:
+        MenuCliente();
+        break;
+      case 3:
+        MenuCorretor();
+        break;
+      case 4:
+        acoes.deletarImovel();
+        break;
+      case 0:
+        console.log("Saindo . . .");
+        process.exit();
+        break;
+      default:
+        console.log("Opção inválida.");
+        break;
+    }
+  }
+};
+
+const MenuCorretora = () => {
+  console.log("GERENCIAMENTO DE CORRETORA");
+  while (true) {
+    console.log(`
+          1- CADASTRAR CORRETORA
+          2- LISTAR CORRETORA
+          3- ATUALIZAR CORRETORA
+          4- EXCLUIR CORRETORA
+          0- SAIR`);
+
+    let opcaoServico = parseInt(prompt(": "));
+    switch (opcaoServico) {
+      case 1:
+        corretora.store();
+        break;
+      case 2:
+        corretora.index();
+        break;
+      case 3:
+        corretora.update();
+        break;
+      case 4:
+        corretora.destroy();
+        break;
+      case 0:
+        return;
+      default:
+        console.log("Opção inválida.");
+        break;
+    }
+  }
+};
+
+const MenuCliente = () => {
+  console.log("GERENCIAMENTO DE CLIENTE");
+  while (true) {
+    console.log(`
+          1- CADASTRAR CLIENTE
+          2- LISTAR CLIENTE
+          3- ATUALIZAR CLIENTE
+          4- EXCLUIR CLIENTE
+          0- SAIR`);
+
+    let opcaoServico = parseInt(prompt(": "));
+    switch (opcaoServico) {
+      case 1:
+        cliente.store();
+        break;
+      case 2:
+        cliente.index();
+        break;
+      case 3:
+        cliente.update();
+        break;
+      case 4:
+        cliente.destroy();
+        break;
+      case 0:
+        return;
+      default:
+        console.log("Opção inválida.");
+        break;
+    }
+  }
+};
+
+const MenuCorretor = () => {
+  console.log("GERENCIAMENTO DE CORRETOR");
+  while (true) {
+    console.log(`
+            1- CADASTRAR CORRETOR
+            2- LISTAR CORRETOR
+            3- ATUALIZAR CORRETOR
+            4- EXCLUIR CORRETOR
+            0- SAIR`);
+
+    let opcaoServico = parseInt(prompt(": "));
+    switch (opcaoServico) {
+      case 1:
+        corretor.store();
+        break;
+      case 2:
+        corretor.index();
+        break;
+      case 3:
+        corretor.update();
+        break;
+      case 4:
+        corretor.destroy();
+        break;
+      case 0:
+        return;
+      default:
+        console.log("Opção inválida.");
+        break;
+    }
+  }
+};
+menuPrincipal();
